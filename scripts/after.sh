e3025051acb83da8db900315b52ee6b04916ac4e
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2020-11-20 14:21:54 +0530

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 sudo mkdir /var/www/dist
 cd /var/www/dist
-aws s3 cp s3://angular-codebuild-cicd/angular/angular.zip /var/www/dist
+aws s3 cp s3://angular-codebuild-cicd/angular.zip /var/www/dist
 unzip angular.zip
 sudo rm -rf angular.zip
 sudo mkdir /var/www/sachin-tendulkar-1

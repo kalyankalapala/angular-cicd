a7b3115b936e0b01dc733dcdde35358df1006ac1
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2020-11-20 15:46:05 +0530

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,9 +1,9 @@
 ### after ###
 
 #!/bin/bash
-sudo mkdir /tmp/csk
-cd /tmp/csk
-aws s3 cp s3://angular-devops-ci-cd/angular.zip /tmp/csk
+sudo mkdir /tmp/sachin-tendulkar
+cd /tmp/sachin-tendulkar
+aws s3 cp s3://angular-devops-ci-cd/angular.zip /tmp/sachin-tendulkar
 #aws s3 cp s3://mydevopscicd/angular.zip .
 unzip angular.zip
 sudo rm -rf angular.zip

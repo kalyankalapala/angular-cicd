a9320646ed2509baf52ee459ef5024cddec91e56
kalyankalapala <kalyankalapala24@gmail.com>
2020-11-23 17:34:15 +0530

    before.sh

---
+++ scripts/before.sh
@@ -0,0 +1,15 @@
+### before ###
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install -y httpd
+
+###install nodejs and angular
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
+sudo yum install -y nodejs
+sudo npm install -g @angular/cli
+sudo npm install -g npm
+
+### aws-configure ###
+#cd /home/ec2-user
+#sudo ./aws-credentials.sh

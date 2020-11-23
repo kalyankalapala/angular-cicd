ae60acec96031803efc88f5b2fa9872cd7882550
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2020-11-23 10:43:02 +0530

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -6,9 +6,9 @@ sudo yum install -y httpd
 
 #### /var/www/dist ####
 cd /var/www
-sudo mkdir /var/www/dist
+sudo rm -rf /var/www/dist
 cd /var/www/dist
-aws s3 cp s3://angular-devops-ci-cd/appspec.yml.txt /var/www/dist
+sudo aws s3 cp s3://angular-devops-ci-cd/appspec.yml.txt /var/www/dist
 #aws s3 cp s3://mydevopscicd/angular.zip .
 unzip angular.zip
 sudo rm -rf angular.zip
@@ -16,7 +16,7 @@ sudo rm -rf angular.zip
 #### /tmp/sachin/tendulkar #####
 sudo mkdir /tmp/sachin-tendulkar
 cd /tmp/sachin-tendulkar
-aws s3 cp s3://angular-devops-ci-cd/angular.zip /tmp/sachin-tendulkar
+sudo aws s3 cp s3://angular-devops-ci-cd/appspec.yml.txt /tmp/sachin-tendulkar
 #aws s3 cp s3://mydevopscicd/angular.zip .
 unzip angular.zip
 sudo rm -rf angular.zip

be40748bc1d76e333dcd4031765a5203bdd980e9
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2020-11-24 11:55:55 +0530

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -6,13 +6,13 @@ sudo yum install -y httpd
 sudo yum install -y mailx
 sudo rm -rf *.txt /var/www/angular
 #### /var/www/dist ####
-#cd /var/www && sudo rm -rf dist/
-#sudo mkdir /var/www/dist
-#cd /var/www/dist && sudo rm -rf *
-#sudo aws s3 cp s3://angular-devops-ci-cd/s3-artifacts.zip /var/www/dist
-#sudo unzip s3-artifacts.zip && sudo rm -rf s3-artifacts.zip
-#sudo rm -rf *.yml scripts
-#cd /var/www/dist/dist/angular-devops/ && sudo mv *.js *.html *.css *.ico /var/www/dist
-#cd /var/www/dist && sudo rm -rf /var/www/dist/dist/
+cd /var/www && sudo rm -rf dist/
+sudo mkdir /var/www/dist
+cd /var/www/dist && sudo rm -rf *
+sudo aws s3 cp s3://angular-devops-ci-cd/s3-artifacts.zip /var/www/dist
+sudo unzip s3-artifacts.zip && sudo rm -rf s3-artifacts.zip
+sudo rm -rf *.yml scripts
+cd /var/www/dist/dist/angular-devops/ && sudo mv *.js *.html *.css *.ico /var/www/dist
+cd /var/www/dist && sudo rm -rf /var/www/dist/dist/
 
 
